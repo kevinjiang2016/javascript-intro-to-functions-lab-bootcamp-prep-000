dbbbb8ab9afa2b6eb8854a3cf6f80e41bbfab310
kevin jiang <kevinjiang2009@yahoo.com>
2018-01-26 01:22:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   } else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   } else {
-    return "I love you,too"
+    return "I love you,too."
   }
 }
 
